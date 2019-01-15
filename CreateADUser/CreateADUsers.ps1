754504e8f41a41cee20e8876e557e41703bdfa68
Smoz <Steve.Moseley@g.austincc.edu>
2019-01-15 16:06:51 -0600

    Update
    small change

--- CreateADUser/CreateADUsers.ps1
+++ CreateADUser/CreateADUsers.ps1
@@ -1,1 +1,3 @@
+#Import CSV for bulk creation
+
 Import-Csv C:\createusers.csv | foreach-object { New-ADUser -Name $_.Name -UserPrincipalName $_.UserPrincipalName -SamAccountName $_.Username -GivenName $_.Givenname -DisplayName $_.Name -Surname $_.Surname -Description $_.Department -EmployeeID $_.EmployeeID -Path $_.Path -ScriptPath $_.ScriptPath -AccountPassword (ConvertTo-SecureString -AsPlainText $_.Password -force) -Enabled $True -PassThru -Country $_.Country -Company $_.Company }; 
